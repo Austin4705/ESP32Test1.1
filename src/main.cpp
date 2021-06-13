9361e8f06e6d251d14ce830817a88b0388bf83cc
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-06-12 23:14:18 -0700

    cleaned up for repo

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 BluetoothSerial SerialBT;
 
 // calibration values for the adc
-#define sampleRatePerSec 1000
 #define DEFAULT_VREF 1100
 esp_adc_cal_characteristics_t *adc_chars;
 
@@ -31,6 +30,7 @@ unsigned long int timer = 0;
 unsigned long int counter = 0;
 unsigned long int startTime = 0;
 
+//Sets up all the adc, bte, and serial
 void setup() {
   Serial.begin(115200);
   initBT();
@@ -61,11 +61,9 @@ void setup() {
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
 
   //------------------------Init ADC------------------------
-
-  // std::queue<uint16_t> internal;
-  // bufferQueue = &internal;
   }
 
+//Initalizes bluetooth
 void initBT(){
   if(!SerialBT.begin("ESP32-Recorder")){
     Serial.println("An error occurred initalizing Bluetooth");
@@ -81,6 +79,7 @@ void initBT(){
   Serial.println("==========================================================");
 }
 
+//Gets Called whenever bluetooth happens
 void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
   if(event == ESP_SPP_SRV_OPEN_EVT){
     Serial.println("Client Connected!");
@@ -96,6 +95,7 @@ void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
     Serial.print(textString);
     Serial.println("\n");
 
+    //If device wants to start or stop call the function
     if(textString.equals("START")){
       startRecording();
     }else if(textString.equals("STOP")){
@@ -104,6 +104,7 @@ void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
   }
 }
 
+//Starts recording by creating the seperate record and transmit loops as well as init the queue
 void startRecording(){
   Serial.println("*** Recording Start ***");
   bufferQueue = new std::queue<uint16_t>;
@@ -112,6 +113,7 @@ void startRecording(){
   //xTaskCreate(logData, "logData", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 }
 
+//Finishes by deleting the recording and creating a new loop that waits for the data to finish sending and then cleans everything up
 void finishRecording(){
   Serial.println("*** Recording End ***");
   vTaskDelete(taskHandler1);
@@ -119,6 +121,7 @@ void finishRecording(){
   xTaskCreate(deviceCancel, "deviceCancel", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 }
 
+//Async log function b/c serial is taxing
 void logData(void *arg){
   vTaskDelay(2000);
   while(1){
@@ -132,21 +135,12 @@ void logData(void *arg){
       Serial.print(", ");
       Serial.println(bufferQueue->size());
 
-      // Serial.print(counter);
-    // Serial.print(", ");
-    // Serial.print((micros() - time2)/1000.00);
-    // Serial.print(", ");
-    // Serial.print(millis() - startTime);
-    // Serial.print(", ");
-    // Serial.print(counter / (((timer - startTime)/1000.00)/1000.00) );
-    // Serial.print(", ");
-    // Serial.println(bufferQueue.size());
     vTaskDelay(2000);
   }
   vTaskDelete(NULL);
 }
 
-
+//A new loop that waits for the data to finish sending and then cleans everything up
 void deviceCancel(void *arg){
   vTaskDelay(2000);
   while(1){
@@ -161,6 +155,7 @@ void deviceCancel(void *arg){
   vTaskDelete(NULL);
 }
 
+//Reads the data from the adc
 void recordData(void *arg){
   Serial.println("Test1");
   timer = micros();
@@ -168,20 +163,17 @@ void recordData(void *arg){
   
   counter = 0;
   while(1){
-    //if(   ((micros() - timer) / 1000.00)/1000.00 >= 1 / (float)(sampleRatePerSec)){//+10.7
       timer = micros();
       int sample = adc1_get_raw(ADC1_CHANNEL_7);
-      // Serial.print("R");
-      //Serial.println(sample);
       counter++;
       bufferQueue->push((uint32_t)sample);
-    //}
     vTaskDelay(portTICK_PERIOD_MS);
 
   }
   vTaskDelete(NULL);
 }
 
+//Transmits the data over bluetooth
 void transmitData(void *arg){
   Serial.println("Test2");
   //unsigned long int startTime2 = millis();
@@ -192,100 +184,18 @@ void transmitData(void *arg){
     flag = 1;
     int sample = (uint32_t)bufferQueue->front();  
     SerialBT.println(sample);
-    //Serial.print("TS");
-    //Serial.println(sample);
     bufferQueue->pop();
     time2 = micros();
     }
     else{
       flag = 0;
-      //Serial.print("TB");
-      //Serial.println(bufferQueue.size());
     }
     vTaskDelay(portTICK_PERIOD_MS/2);
   }
   vTaskDelete(NULL);
 }
 
+//Not needed but requried to compile
 void loop()
 {
 }
-
-
-
-
-
-
-
-
-
-// 
-
-//Deprecated
-
-// void i2s_adc(void *arg)
-// {
-//     Serial.println(" *** Recording Start *** ");
-//     while (1) {
-//         // for a more accurate reading you could read multiple samples here
-
-//         int sample = adc1_get_raw(ADC1_CHANNEL_7);
-//         Serial.println(sample);
-//     }
-
-//     vTaskDelete(NULL);
-// }
-
-// void i2sInit(){
-//   i2s_config_t i2s_config = {
-//     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-//     .sample_rate = I2S_SAMPLE_RATE,
-//     .bits_per_sample = i2s_bits_per_sample_t(I2S_SAMPLE_BITS),
-//     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-//     .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-//     .intr_alloc_flags = 0,
-//     .dma_buf_count = 8,
-//     .dma_buf_len = 64,
-//     .use_apll = 1
-//   };
-
-//   i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
-
-//   const i2s_pin_config_t pin_config = {
-//     .bck_io_num = I2S_SCK,
-//     .ws_io_num = I2S_WS,
-//     .data_out_num = -1,
-//     .data_in_num = I2S_SD
-//   };
-
-//   i2s_set_pin(I2S_PORT, &pin_config);
-// }
-
-// void i2s_adc_data_scale(uint8_t * d_buff, uint8_t* s_buff, uint32_t len)
-// {
-//     uint32_t j = 0;
-//     uint32_t dac_value = 0;
-//     for (int i = 0; i < len; i += 2) {
-//         dac_value = ((((uint16_t) (s_buff[i + 1] & 0xf) << 8) | ((s_buff[i + 0]))));
-//         d_buff[j++] = 0;
-//         d_buff[j++] = dac_value * 256 / 2048;
-//     }
-// }
-// Task to write samples to our server
-// void writerTask(void *param)
-// {
-//   sampler = (I2SSampler *)param;
-//   const TickType_t xMaxBlockTime = pdMS_TO_TICKS(100);
-//   while (true)
-//   {
-//     // wait for some samples to save
-//     uint32_t ulNotificationValue = ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
-//     if (ulNotificationValue > 0)
-//     {
-//       // send them off to the server
-//        SerialBT.println((int)sampler->sampleBuffer());
-//     }
-//   }
-// }
-
-
